768e239c9afa3d614951334dff90c634e0f50b18
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-02-19 17:26:06 +0100

    Add loader to EventsPage

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,6 +1,51 @@
 import React from 'react';
 import { Heading } from '@chakra-ui/react';
+import { Link, useLoaderData } from 'react-router-dom';
+
+//Display events
+export const loader = async () => {
+  const events = await fetch("http://localhost:3000/events");
+  const categories = await fetch("http://localhost:3000/categories");
+  
+
+  return { 
+    events: await events.json(), 
+    categories: await categories.json()
+  };
+};
 
 export const EventsPage = () => {
-  return <Heading>List of events</Heading>;
+  const {events, categories} = useLoaderData()
+  
+
+  const categoryId = categories.map((category) => {
+    return category.id
+  })
+
+console.log (categoryId)
+
+  return (
+    <div>
+    <Heading>List of events</Heading>
+    {events.map((event) => (
+      <div key={event.id}>
+        <Link to={`event/${event.id}`}>
+          <h2>{event.title}</h2>
+        </Link>
+        <p>
+          Description {" "}
+          {event.description}
+        </p>
+        <p>  
+          {event.image}
+        </p>
+        <p>
+          Starttime:{" "}{event.startTime}
+          Endtime: {" "}{event.endTime}
+        </p>
+          Categories:{" "}            
+      </div>
+    ))}
+    </div>
+  );
 };
