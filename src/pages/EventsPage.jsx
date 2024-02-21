d3b20a9917d4db7422e307a6e0ee2847c7ade9b6
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-02-21 15:25:39 +0100

    Add Card

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,51 +1,54 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/react';
-import { Link, useLoaderData } from 'react-router-dom';
+import React from "react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  SimpleGrid,
+} from "@chakra-ui/react";
+import { Link, useLoaderData } from "react-router-dom";
 
 //Display events
 export const loader = async () => {
   const events = await fetch("http://localhost:3000/events");
   const categories = await fetch("http://localhost:3000/categories");
-  
 
-  return { 
-    events: await events.json(), 
-    categories: await categories.json()
+  return {
+    events: await events.json(),
+    categories: await categories.json(),
   };
 };
 
 export const EventsPage = () => {
-  const {events, categories} = useLoaderData()
-  
-
-  const categoryId = categories.map((category) => {
-    return category.id
-  })
-
-console.log (categoryId)
+  const { events, categories } = useLoaderData();
 
   return (
-    <div>
-    <Heading>List of events</Heading>
-    {events.map((event) => (
-      <div key={event.id}>
-        <Link to={`event/${event.id}`}>
-          <h2>{event.title}</h2>
-        </Link>
-        <p>
-          Description {" "}
-          {event.description}
-        </p>
-        <p>  
-          {event.image}
-        </p>
-        <p>
-          Starttime:{" "}{event.startTime}
-          Endtime: {" "}{event.endTime}
-        </p>
-          Categories:{" "}            
-      </div>
-    ))}
-    </div>
+    <>
+      <Heading>List of events</Heading>
+      <SimpleGrid spacing={10} minChildWidth={"300px"}>
+        {events &&
+          events.map((event) => (
+            <Card key={event.id}>
+              <Link to={`event/${event.id}`}>
+                <CardHeader>{event.title}</CardHeader>
+              </Link>
+              <CardBody>
+                <p>Description {event.description}</p>
+                <p>{event.image}</p>
+                <p>
+                  Starttime: {event.startTime}
+                  Endtime: {event.endTime}
+                </p>
+                Categories:{" "}
+                {categories.map((category) =>
+                  event.categoryIds?.includes(category.id) ? (
+                    <>{category.name}</>
+                  ) : null
+                )}
+              </CardBody>
+            </Card>
+          ))}
+      </SimpleGrid>
+    </>
   );
 };
