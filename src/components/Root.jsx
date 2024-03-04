b68b212b291bfebc7bebb1a016151d9b5029f94a
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-03-04 15:00:38 +0100

    Change Root BG color

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -5,7 +5,7 @@ import { Box } from "@chakra-ui/react";
 
 export const Root = () => {
   return (
-    <Box backgroundColor="blue.900" maxWidth="100vw" minHeight="100vh">
+    <Box backgroundColor="#314447" maxWidth="100vw" minHeight="100vh">
       <Navigation />
       <Outlet />
     </Box>
