25cc0b0427e2a5706e349a770369222e0124cdbe
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-03-06 18:02:15 +0100

    Add loader to EventPage

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,6 +1,59 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import React from "react";
+import {
+  Box,
+  Card,
+  CardBody,
+  Container,
+  Heading,
+  Image,
+  Text,
+} from "@chakra-ui/react";
+import { useLoaderData } from "react-router-dom";
+
+export const loader = async ({ params }) => {
+  const event = await fetch(`http://localhost:3000/events/${params.eventId}`);
+  const categories = await fetch("http://localhost:3000/categories");
+  const users = await fetch("http://localhost:3000/users");
+
+  return {
+    event: await event.json(),
+    categories: await categories.json(),
+    users: await users.json(),
+  };
+};
 
 export const EventPage = () => {
-  return <Heading>Event</Heading>;
+  const { event, categories, users } = useLoaderData();
+
+  return (
+    <Container padding="0px" ml="auto" mr="auto">
+      <Heading align="center" color="white" mt="20px" mb="60px">
+        {event.title}
+      </Heading>
+      <Card
+        size="lg"
+        bgColor="white"
+        direction={{ base: "column", sm: "row" }}
+        variant="outline"
+      >
+        <Image
+          h="400px"
+          objectFit="cover"
+          maxW={{ base: "100%", sm: "200px" }}
+          src={event.image}
+        />
+
+        <CardBody>
+          <Text
+            mt="10px"
+            fontSize="18px"
+            fontStyle="italic"
+            fontWeight="semibold"
+          >
+            {event.description}
+          </Text>
+        </CardBody>
+      </Card>
+    </Container>
+  );
 };
