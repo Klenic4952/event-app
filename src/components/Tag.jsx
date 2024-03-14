db59e191883b9d9cb5a0f4b387226ec81e881b4e
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-03-14 12:28:50 +0100

    Create Tag Component

---
+++ src/components/Tag.jsx
@@ -0,0 +1,20 @@
+import { Tag as CTag } from "@chakra-ui/react";
+
+export const Tag = ({ children, ...props }) => {
+  return (
+    <CTag
+      p="8px"
+      margin="1.5"
+      mb="5"
+      mt="5"
+      backgroundColor="#A5A726"
+      size="md"
+      variant="solid"
+      textTransform="uppercase"
+      fontWeight="bold"
+      {...props}
+    >
+      {children}
+    </CTag>
+  );
+};
