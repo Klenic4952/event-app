9411f49616a321175a755ed3cfbe0fa60e9c4c62
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-03-21 20:04:08 +0100

    Style Nav

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,15 +1,34 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { AddIcon, ChevronLeftIcon } from "@chakra-ui/icons";
+import { Box, Button, Flex } from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
 
+
+
 export const Navigation = () => {
+
   return (
-    <Flex as="nav" gap="10px" justifyContent={"flex-end"} color="white">
+    <Flex
+      as="nav"
+      gap="10px"
+      justifyContent="flex-end"
+      color="white"
+      fontSize="22px"
+      p="15px"
+    >
       <Box>
-        <Link to="/">Events</Link>
+        <Link to="/">
+          <Button variant="ghost" size="lg" leftIcon={<ChevronLeftIcon/>}_hover="none">Events</Button>
+        </Link>
       </Box>
       <Box>
-        <Link to="/event/1">Event</Link>
+        <Link to="/addevents">
+          <Button variant="ghost" size="lg" leftIcon={<AddIcon boxSize="12px"/>} _hover={{ bg: "white", color: "#314447" }} >
+            Event
+          </Button>
+        </Link>
+        
+    
       </Box>
     </Flex>
   );
