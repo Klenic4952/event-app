a1274e22533e212fc89a248c622af32d93637255
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-04-11 12:06:20 +0200

    Create Events Component

---
+++ src/components/Events.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import {
+  Box,
+  Card,
+  CardBody,
+  Flex,
+  Heading,
+  Image,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
+import { Tag } from "../components/Tag";
+import { Link } from "react-router-dom";
+
+export const Events = ({ events, categories }) => {
+  return (
+    <SimpleGrid spacing={10} minChildWidth={"250px"}>
+      {events &&
+        events.map((event) => (
+          <Card
+            key={event.id}
+            maxW="sm"
+            borderRadius="xl"
+            cursor="pointer"
+            _hover={{ transform: "scale(1.05)" }}
+            transition="transform .4s"
+          >
+            <Link to={`event/${event.id}`}>
+              <CardBody padding="0" align="center">
+                <Image
+                  h="225px"
+                  w="full"
+                  objectFit="cover"
+                  borderTopRadius="xl"
+                  src={event.image}
+                />
+                <Heading fontSize="28px" mt="10px" color="#803419">
+                  {event.title}
+                </Heading>
+                <Text
+                  mt="10px"
+                  fontSize="18px"
+                  fontStyle="italic"
+                  fontWeight="semibold"
+                >
+                  {event.description}
+                </Text>
+                <Flex flexDir="row" gap="1.5" justify="center" mt="5">
+                  <Box>Start Time:</Box>
+                  <Box fontWeight="semibold">
+                    {event.startTime.substring(0, 10)}{" "}
+                    {event.startTime.substring(11, 16)}
+                  </Box>
+                </Flex>
+                <Flex flexDir="row" gap="1.5" justify="center">
+                  <Box>End Time:</Box>
+                  <Box fontWeight={"semibold"}>
+                    {event.endTime.substring(0, 10)}{" "}
+                    {event.endTime.substring(11, 16)}
+                  </Box>
+                </Flex>
+                <Box mt="5px">
+                  {event.categoryIds.map((id) => (
+                    <Tag key={id}>
+                      {categories.find((category) => category.id === id)?.name}
+                    </Tag>
+                  ))}
+                </Box>
+              </CardBody>
+            </Link>
+          </Card>
+        ))}
+    </SimpleGrid>
+  );
+};
