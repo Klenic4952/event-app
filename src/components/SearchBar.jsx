4691f8025ef7c034472e8b8ed7aea57cac8e66ab
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-04-22 12:26:29 +0200

    Delete unused props

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,16 +6,13 @@ export const SearchBar = ({ onChange }) => {
   return (
     <Input
       backgroundColor="white"
-      focusBorderColor="#38A169"
+      focusBorderColor="white"
       display="flex"
-      alignItems="center"
-      justifyContent="center"
       p="10px"
       mb={{ base: "30px", md: "35px" }}
-      ml="5px"
       width={{ base: "300px", sm: "350px", md: "400px" }}
       type="text"
-      placeholder="Search here for events"
+      placeholder="Search for events"
       fontSize="18px"
       onChange={onChange}
     />
