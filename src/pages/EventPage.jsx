7f44bc59ed32afaa8acc1982ee1f977d26180dce
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-04-22 19:52:49 +0200

    Add responsiveness

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "@chakra-ui/react";
+import { Center} from "@chakra-ui/react";
 import { useLoaderData } from "react-router-dom";
 import { Event } from "../components/Event";
 
@@ -19,14 +19,14 @@ export const EventPage = () => {
   const { event, categories, users } = useLoaderData();
 
   return (
-    <Container
-      maxWidth="100vh"
+    <Center
+      maxWidth={{ base: "95vw", sm: "90vw", lg: "100vw" }}
       minHeight="100vh"
       padding="0px"
       ml="auto"
       mr="auto"
     >
       <Event event={event} categories={categories} users={users}></Event>
-    </Container>
+    </Center>
   );
 };
