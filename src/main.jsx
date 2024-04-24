8d0a648a6fe63e9b5a359e812215fc86ecf209b1
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-04-24 17:38:55 +0200

    Replace pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,46 +1,43 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { EventPage, loader as eventLoader} from './pages/EventPage';
-import { EventsPage, loader as eventListLoader  } from './pages/EventsPage';
-import { AddEvent, loader as addEventLoader } from './components/AddEvent';
-import { EditEvent, loader as editEventLoader } from './components/EditEvent';
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { EventPage, loader as eventLoader } from "./pages/EventPage";
+import { EventsPage, loader as eventListLoader } from "./pages/EventsPage";
+import { AddEvent, loader as addEventLoader } from "./pages/AddEvent";
+import { EditEvent, loader as editEventLoader } from "./pages/EditEvent";
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Root } from './components/Root';
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Root } from "./components/Root";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Root />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <EventsPage />,
         loader: eventListLoader,
       },
       {
-        path: '/event/:eventId',
+        path: "/event/:eventId",
         element: <EventPage />,
         loader: eventLoader,
       },
-      {path: '/event/:eventId/editevent',
+      {
+        path: "/event/:eventId/editevent",
         element: <EditEvent />,
         loader: editEventLoader,
       },
-      {path: '/addevent',
-      element: <AddEvent />,
-      loader: addEventLoader,
-    },
+      { path: "/addevent", element: <AddEvent />, loader: addEventLoader },
     ],
   },
 ]);
 // @ts-ignore
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
       <RouterProvider router={router} />
     </ChakraProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
