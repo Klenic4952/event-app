0a3846e7634077569d16052d99f99125a12fe41c
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-04-24 14:03:59 +0200

    Import EventForm

--- src/components/EditEvent.jsx
+++ src/components/EditEvent.jsx
@@ -1,19 +1,8 @@
-import {
-  Button,
-  Center,
-  Checkbox,
-  Flex,
-  FormControl,
-  FormLabel,
-  Heading,
-  Input,
-  Select,
-  Text,
-  useToast,
-} from "@chakra-ui/react";
+import { Center, useToast } from "@chakra-ui/react";
 import React, { useEffect } from "react";
-import { Form, useNavigate, useLoaderData } from "react-router-dom";
+import { useNavigate, useLoaderData } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { EventForm } from "./EventForm";
 
 // loader function to get event, categories and users for the form
 export const loader = async ({ params }) => {
@@ -98,10 +87,15 @@ export const EditEvent = () => {
     // ensure createdBy is a number
     const createdByNumber = parseInt(createdBy);
 
-    // ensure categoryIds is an array of numbers
-    const categoryIdsArray = Array.isArray(categoryIds)
-      ? categoryIds.map((id) => parseInt(id))
-      : [parseInt(categoryIds)];
+    // ensure categoryIds gets only updated if new values
+    const categoryIdsSet = new Set(
+      Array.isArray(categoryIds)
+        ? categoryIds.map((id) => parseInt(id))
+        : [parseInt(categoryIds)]
+    );
+
+    // convert Set back to an array
+    const categoryIdsArray = Array.from(categoryIdsSet);
 
     const editEventData = {
       ...otherFormData,
@@ -128,18 +122,17 @@ export const EditEvent = () => {
           position: "top",
         });
       } else {
-
-      toast({
-        title: "Event updated",
-        description: "We have successfully edited the event for you!",
-        status: "success",
-        duration: 5000,
-        isClosable: true,
-        position: "top",
-      });
-      // use the navigate function to go to updated page
-      navigate(`/event/${event.id}`);
-    }
+        toast({
+          title: "Event updated",
+          description: "We have successfully edited the event for you!",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+        });
+        // use the navigate function to go to updated page
+        navigate(`/event/${event.id}`);
+      }
     } catch (error) {
       // handle any errors that might occur during this process
       setError("root", {
@@ -148,185 +141,22 @@ export const EditEvent = () => {
     }
   };
 
-  //styles for the labels and inputfields
-  const inputStyles = {
-    width: { base: "350px", sm: "450px", md: "500px", lg: "800px" },
-    bg: "#e6edff",
-    fontSize: { base: "14px", sm: "15px", lg: "16px" },
-    fontWeight: "semibold",
-    color: "#314447",
-  };
-
-  const labelStyles = {
-    width: { base: "325px", sm: "425px", md: "475px", lg: "750px" },
-    fontSize: { base: "15px", sm: "17px", lg: "18px" },
-    mt: "25px",
-  };
-
   return (
-    <Center align="center" p="10px" pb="30px" color="whiteAlpha.800">
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <Heading color="#A5A726">Edit Event</Heading>
-        <FormControl mt="15px">
-          <FormLabel sx={labelStyles}>Title</FormLabel>
-          <Input
-            {...register("title", { required: true })}
-            type="text"
-            placeholder="Title"
-            sx={inputStyles}
-            focusBorderColor="white"
-          />
-          {errors.title && (
-            <Text mt="8px" color="red">
-              Please give a title to the event!
-            </Text>
-          )}
-        </FormControl>
-
-        <FormControl mt="15px">
-          <FormLabel sx={labelStyles}>Description</FormLabel>
-          <Input
-            {...register("description", { required: true })}
-            type="text"
-            placeholder="Description"
-            sx={inputStyles}
-            focusBorderColor="white"
-          />
-          {errors.description && (
-            <Text mt="8px" color="red">
-              Please give a short description to the event!
-            </Text>
-          )}
-        </FormControl>
-
-        <FormControl mt="15px">
-          <FormLabel sx={labelStyles}>Image</FormLabel>
-          <Input
-            {...register("image", { required: true })}
-            type="url"
-            placeholder="Url"
-            sx={inputStyles}
-            focusBorderColor="white"
-          />
-          {errors.image && (
-            <Text mt="8px" color="red">
-              Please add a valid image-url to the event!
-            </Text>
-          )}
-        </FormControl>
-
-        <FormControl mt="15px">
-          <FormLabel sx={labelStyles}>Location</FormLabel>
-          <Input
-            {...register("location", { required: true })}
-            type="text"
-            placeholder="Location"
-            sx={inputStyles}
-            focusBorderColor="white"
-          />
-          {errors.location && (
-            <Text mt="8px" color="red">
-              Please add the location to the event!
-            </Text>
-          )}
-        </FormControl>
-
-        <FormControl mt="15px">
-          <FormLabel sx={labelStyles}>Start time:</FormLabel>
-          <Input
-            {...register("startTime", { required: true })}
-            type="datetime-local"
-            sx={inputStyles}
-            focusBorderColor="white"
-          />
-          {errors.startTime && (
-            <Text mt="8px" color="red">
-              Please add the date and start time to the event!
-            </Text>
-          )}
-        </FormControl>
-
-        <FormControl mt="15px">
-          <FormLabel sx={labelStyles}>End time</FormLabel>
-          <Input
-            {...register("endTime", { required: true })}
-            type="datetime-local"
-            sx={inputStyles}
-            focusBorderColor="white"
-          />
-          {errors.endTime && (
-            <Text mt="8px" color="red">
-              Please add the date and end time to the event!
-            </Text>
-          )}
-        </FormControl>
-
-        <FormControl mt="15px">
-          <FormLabel sx={labelStyles}>Categories:</FormLabel>
-          <Flex justifyContent="center" gap="20px">
-            {categories.map(({ name, id }) => (
-              <FormLabel
-                fontSize={{ base: "15px", sm: "17px", lg: "18px" }}
-                key={id}
-                mt="10px"
-              >
-                {name.charAt(0).toUpperCase() + name.slice(1)}
-                <Checkbox
-                  {...register("categoryIds", { required: true })}
-                  type="checkbox"
-                  id={id}
-                  value={id}
-                  mt={{ base: "4px", sm: "6px", lg: "7px" }}
-                  ml="7px"
-                  colorScheme="blackAlpha"
-                />
-                {errors.categoryIds && (
-                  <Text mt="8px" color="red" fontSize="15px">
-                    Please choose at least one categorie!
-                  </Text>
-                )}
-              </FormLabel>
-            ))}
-          </Flex>
-        </FormControl>
-
-        <FormControl mt="15px">
-          <FormLabel sx={labelStyles}>Created by:</FormLabel>
-          <Select
-            placeholder="Select a user"
-            color="#314447"
-            {...register("createdBy", { required: "true" })}
-            mt="15px"
-            sx={inputStyles}
-            focusBorderColor="white"
-          >
-            {users.map((user) => (
-              <option key={user.id} value={user.id}>
-                {user.name}
-              </option>
-            ))}
-          </Select>
-          {errors.createdBy && (
-            <Text sx="inputStyles" mt="8px" color="red" fontSize="15px">
-              Please select a user!
-            </Text>
-          )}
-        </FormControl>
-        <Button
-          size={{ base: "md", lg: "lg" }}
-          disabled={isSubmitting}
-          type="submit"
-          bgColor="#A5A726"
-          mt="30px"
-        >
-          {isSubmitting ? "Editing event..." : "Edit Event"}
-        </Button>
-        {errors.root && (
-          <Text mt="8px" color="red" fontSize="15px">
-            {errors.root.message}
-          </Text>
-        )}
-      </Form>
+    <Center
+      align="center"
+      flexDirection="column"
+      p="10px"
+      pb="30px"
+      color="whiteAlpha.800"
+    >
+      <EventForm
+        handleSubmit={handleSubmit(onSubmit)}
+        register={register}
+        errors={errors}
+        categories={categories}
+        users={users}
+        isSubmitting={isSubmitting}
+      />
     </Center>
   );
 };
