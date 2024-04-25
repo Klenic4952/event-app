84b089ecb8c20fbefed4b02f7e9a56c963499f1b
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-04-25 13:57:43 +0200

    Add ErrorBoundary

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { Navigation } from "./Navigation";
 import { Box } from "@chakra-ui/react";
+import ErrorBoundary from "./ErrorBoundary";
 
 export const Root = () => {
   return (
-    <Box backgroundColor="#314447" maxWidth="100vw" minHeight="100vh">
-      <Navigation />
-      <Outlet />
-    </Box>
+    <ErrorBoundary>
+      <Box backgroundColor="#314447" maxWidth="100vw" minHeight="100vh">
+        <Navigation />
+        <Outlet />
+      </Box>
+    </ErrorBoundary>
   );
 };
