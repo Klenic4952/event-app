62e6e9a4a0868cb3513c7d7f06afecb525bf3ff0
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-04-25 13:59:13 +0200

    Update ErrorBoundary

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -1,28 +1,46 @@
+import { ChevronLeftIcon } from "@chakra-ui/icons";
+import { Button, Center, Heading, Text } from "@chakra-ui/react";
 import { Component } from "react";
+import { Link } from "react-router-dom";
 
 class ErrorBoundary extends Component {
-    constructor(props) {
-      super(props);
-      this.state = { hasError: false };
-    }
-  
-    static getDerivedStateFromError(error) {
-      // Update state so the next render will show the fallback UI.
-      return { hasError: true, error };
-    }
-  
-    componentDidCatch(error, info) {
-        console.log(error, info)
-    }
-    
-    render() {
-      if (this.state.hasError) {
-        // You can render any custom fallback UI
-        return this.props.fallback;
-      }
-  
-      return this.props.children;
-    }
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null };
   }
 
-  export default ErrorBoundary;
+  static getDerivedStateFromError(error) {
+    return { hasError: true, error };
+  }
+
+  componentDidCatch(error, info) {
+    console.log(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <Center color="white" flexDirection="column">
+          <Heading mt="20">Error</Heading>
+          <Text mt="20" mb="20" fontStyle="italic" fontSize="25">
+            {"Something went wrong"}
+          </Text>
+          <Link to="/">
+            <Button
+              variant="ghost"
+              size="lg"
+              leftIcon={<ChevronLeftIcon />}
+              _hover="none"
+            >
+              Back to events
+            </Button>
+          </Link>
+        </Center>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
